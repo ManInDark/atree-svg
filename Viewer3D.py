f402596bf83621dd396664183853634704037de8
ManInDark <61268856+ManInDark@users.noreply.github.com>
2022-01-25 10:00:35 +0100

    Solved problem caused by exception raised by points on the same x-coordinate as the eye

--- Viewer3D.py
+++ Viewer3D.py
@@ -63,15 +63,18 @@ class Viewer3D:
         self.eye_plane_distance = drawingplane.point.x - eye.x
 
     def see(self, p: Point3D) -> Point2D:
-        my = (self.eye.y - p.y) / (self.eye.x - p.x)
-        mz = (self.eye.z - p.z) / (self.eye.x - p.x)
-        y = self.eye.y + my * self.eye_plane_distance - self.plane.point.y
-        if not (y >= 0 and y < self.plane.height):
-            raise OutOfView("y", y, f"{0}..{self.plane.height}")
-        z = self.eye.z + mz * self.eye_plane_distance - self.plane.point.z
-        if not (z >= 0 and z < self.plane.width):
-            raise OutOfView("z", z, f"{0}..{self.plane.width}")
-        return Point2D(z, y)
+        try:
+            my = (self.eye.y - p.y) / (self.eye.x - p.x)
+            mz = (self.eye.z - p.z) / (self.eye.x - p.x)
+            y = self.eye.y + my * self.eye_plane_distance - self.plane.point.y
+            if not (y >= 0 and y < self.plane.height):
+                raise OutOfView("y", y, f"{0}..{self.plane.height}")
+            z = self.eye.z + mz * self.eye_plane_distance - self.plane.point.z
+            if not (z >= 0 and z < self.plane.width):
+                raise OutOfView("z", z, f"{0}..{self.plane.width}")
+            return Point2D(z, y)
+        except ZeroDivisionError:
+            raise OutOfView("x", 0, "0..∞")
 
     def translatePoint(self, p: Point2D) -> Point2D:
         return self.plane.translatePoint(p)
