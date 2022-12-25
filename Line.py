aeae8c24704925592c93917047712d8121137416
ManInDark <61268856+ManInDark@users.noreply.github.com>
2022-12-25 11:17:16 +0100

    Added GitHub pages

--- Line.py
+++ Line.py
@@ -1,5 +1,5 @@
 from Object import Object, OutOfPoints
-from Viewer3D import Point3D, Viewer3D, Plane
+from Viewer3D import Point3D, Point2D, Viewer3D, Plane
 
 
 class Line(Object):
@@ -24,8 +24,8 @@ class Line(Object):
 
 
 if __name__ == "__main__":
-    def c_list(v: Viewer3D, c: Object):
-        list = []
+    def c_list(v: Viewer3D, c: Object) -> list[Point2D]:
+        list: list[Point2D] = []
         while True:
             try:
                 list.append(v.translatePoint(v.see(c.nextPoint())))
@@ -34,9 +34,7 @@ if __name__ == "__main__":
         return list
     list = c_list(Viewer3D(Point3D(0, 5, 5), Plane(Point3D(10, 0, 0), 10, 10)),
                   Line(Point3D(10, 10, 10), 8, Point3D(10, 0, 10)))
-    s = ""
-    for e in list:
-        s += e.pathPrint("L") + " "
+    s = " ".join([e.pathPrint("L") for e in list])
     print("<style>body{margin:0px;}path{stroke-linecap:round;fill: none;stroke-width:1px; d:path('" +
           s.replace("L", "M", 1)+"');}</style>")
     print('<svg viewBox="0 0 10 10" height="100vh"><path stroke="red"/></svg>')
